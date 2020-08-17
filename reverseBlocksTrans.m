c717a584c1dd538abc608c5be1986b98be1d24b8
Yago Lucas de Souza Freitas <yago.freitas@vortex.net.br>
2020-08-17 19:40:55 -0300

    separar macroblocos

---
+++ reverseBlocksTrans.m
@@ -0,0 +1,12 @@
+function macroBlocos = reverseBlocksTrans(imagem,crop) 
+    copia = imagem;
+    copia = imresize(copia,[320 280]);
+    [width, height] = size(copia);
+    for i = crop:crop:width
+        for j = crop:crop:height
+            copia(i-crop+1:i, j-crop+1:j) = idct2(copia(i-crop+1:i, j-crop+1:j));
+        end
+    end
+    imshow(copia);
+    macroBlocos = copia;
+end
